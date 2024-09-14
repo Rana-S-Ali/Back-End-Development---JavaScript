56a2b05d70512ff78f8b80a4371029d822c87bbf
Rana <ranasar@gmail.com>
2024-09-13 21:58:27 -0400

    third commit

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+
+//var generateName = require("sillyname");
+//import generateName from "sillyName";
+import  generateName from "sillyName";
+
+var sillyName = generateName();
+//console.log(`My name is ${sillyName}.`);
+console.log("My name is " + sillyName);
+//console.log(`My name is ${sillyName}.`);
